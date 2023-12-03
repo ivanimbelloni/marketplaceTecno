13ae97b7ecaa0246cffd9ea121ad669da0415778
ivan imbelloni <ivanimbelloni@gmail.com>
2023-12-03 19:27:43 -0300

    arreglos esteticos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import CardWidget from "./CartWidget"
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import data from "../data/products.json";
 import Logo from "../img/TecnoMarket_logo.svg"
+import ContactoIcon from "../img/contact.svg"
 const categories = data.map((item) => item.category);
 const uniqueCategories = new Set(categories);
 
@@ -10,15 +11,14 @@ function NavBar() {
   return (
     <>
     <header>
-      <img src={Logo} alt="Tecno market" style={{width:"50px"}}/>
       <div className="navbar">
-
+        <div className="navbar-logo">
+      <Link to={'/'}>
+        <img src={Logo} alt="Tecno market" style={{width:"50px"}}/>
+      </Link>
+        </div>
+      <div className="navbar-category">
         <ul>
-          <NavLink to="/">
-            <li key="inicio">
-              <span> Inicio </span>
-            </li>
-          </NavLink>
                 {[...uniqueCategories].map(category => (
                     <NavLink to={`/products/${category}`}>
                   <li key={category}>
@@ -26,14 +26,15 @@ function NavBar() {
                   </li>
                     </NavLink>
                   ))}
-          <NavLink to="/contacto">
-          <li key="contacto">
-              <span> Contacto </span>
-          </li>
-            </NavLink>
         </ul>
       </div>
+      <div className="navbar-right">
+      <Link to={'/contacto'}>
+        <img src={ContactoIcon} alt="Contacto" style={{width:"24px"}}/>
+      </Link>
       <CardWidget/>
+      </div>
+      </div>
     </header>
     </>
 
